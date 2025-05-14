2777b322e3c1318ca431bfcf6ed302e039462efa
natanpms <natanportoms@outlook.com>
2025-05-13 23:30:23 -0300

    feat: adicionando mapa de visualização de localização

--- src/App.svelte
+++ src/App.svelte
@@ -2,36 +2,58 @@
   import { Capacitor } from "@capacitor/core";
   import LocationWeb from "./lib/LocationWeb.svelte";
 
-  interface LocationData {
-    label: string;
-    value: string;
-  }
-
-  let locationData: LocationData[] = [];
-
-  // Verifica se está em uma plataforma nativa
   const isNative: boolean = Capacitor.isNativePlatform();
 
- 
+  let locationData = [];
+  let position: { lat: number; lng: number } | null = null;
 </script>
 
 <main>
   <div class="flex flex-col justify-center items-center min-h-screen">
     <h1 class="text-3xl font-bold">Check-In</h1>
-    <!-- card -->
-    <div class="m-2 p-8 shadow-md w-2/3 rounded-md h-auto bg-white">
-      <h2 class="text-2xl font-semibold text-left">Dados de Geolocalização:</h2>
+
+    <div class="m-2 p-8 shadow-md w-4/5 rounded-md h-auto bg-white">
+      <h2 class="text-2xl mb-4 text-center font-semibold ">Dados de Geolocalização:</h2>
 
       {#if isNative}
         <p>teste</p>
       {:else}
-        <LocationWeb />
+        <LocationWeb bind:locationData bind:position />
       {/if}
-      
 
-      <button class="btn btn-accent captureButton">Capturar Foto</button>
-      <canvas id="canvas" style="display:none;"></canvas>
-      <img id="photoPreview" alt="Preview da foto" />
+      {#if locationData.length > 0}
+        {#each locationData as { label, value }}
+          <p class="text-center m-2 font-sm p-1">
+            <strong>{label}:</strong>
+            {value}
+          </p>
+        {/each}
+      {/if}
+
+      {#if position}
+        <iframe
+          title="locationMap"
+          width="100%"
+          height="450"
+          frameborder="0"
+          scrolling="no"
+          marginheight="0"
+          marginwidth="0"
+          src={"https://www.openstreetmap.org/export/embed.html?" +
+            `bbox=${position.lng - 0.005},${position.lat - 0.005},${position.lng + 0.005},${position.lat + 0.005}` +
+            `&marker=${position.lat},${position.lng}`}
+          style="border: 1px solid black"
+        ></iframe>
+        <br />
+        <small>
+          <a class="font-semibold hover:underline"
+            href={`https://www.openstreetmap.org/?mlat=${position.lat}&mlon=${position.lng}#map=15/${position.lat}/${position.lng}`}
+            target="_blank"
+          >
+            Ver mapa maior
+          </a>
+        </small>
+      {/if}
     </div>
   </div>
 </main>
--- src/lib/LocationWeb.svelte
+++ src/lib/LocationWeb.svelte
@@ -1,65 +1,35 @@
 <script lang="ts">
-  // Componente de localização da web
-
   import { onMount } from "svelte";
 
-  interface LocationData {
-    label: string;
-    value: string;
-  }
+  export let position: { lat: number; lng: number } | null = null;
+  export let locationData: Array<{ label: string; value: string }> = [];
 
-  let locationData: LocationData[] = [];
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          const { latitude, longitude, accuracy, speed } = pos.coords;
 
-  if ("geolocation" in navigator) {
-    console.log("Geolocalização disponível");
+          locationData = [
+            { label: "Latitude", value: latitude.toString() },
+            { label: "Longitude", value: longitude.toString() },
+            { label: "Precisão", value: `${accuracy} m` },
+            { label: "Velocidade", value: (speed ?? "0").toString() },
+          ];
 
-    function getLocation() {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            locationData = [
-              {
-                label: "Latitude",
-                value: position.coords.latitude.toString(),
-              },
-              {
-                label: "Longitude",
-                value: position.coords.longitude.toString(),
-              },
-              { label: "Precisão", value: `${position.coords.accuracy}  m` },
-              {
-                label: "Velocidade",
-                value: (position.coords.speed ?? "0").toString(),
-              },
-            ];
-          },
-          (error) => {
-            console.error(error);
-          },
-        );
-      } else {
-        alert("Geolocalização não é suportada neste navegador.");
-      }
+          position = {
+            lat: latitude,
+            lng: longitude,
+          };
+        },
+        (error) => {
+          alert(error);
+        }
+      );
     }
-
-    // Chama a função de geolocalização quando o componente é montado
-    onMount(() => {
-      getLocation();
-    });
-  } else {
-    alert(
-      "I'm sorry, but geolocation services are not supported by your browser.",
-    );
   }
-</script>
 
-<div>
-  {#if locationData.length > 0}
-    {#each locationData as { label, value }}
-      <p class="font-sm p-1">
-        <strong>{label}:</strong>
-        {value}
-      </p>
-    {/each}
-  {/if}
-</div>
+  onMount(() => {
+    getLocation();
+  });
+</script>
